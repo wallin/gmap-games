dfa54968f3780e14a30eb88ceea67903a47e4556
Sebastian Wallin <sebastian.wallin@gmail.com>
2011-07-31 17:58:02 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Playing with google maps (using frank).
+
+* Polygons with holes.
+* fast marker strategies
+* animations
---
+++ static/js/application.js
@@ -0,0 +1,264 @@
+window.GM = window.GM || {};
+
+var malmo = {
+  "type": "Feature",
+  "bbox": [12.7153, 55.4892, 13.1500, 55.6797],
+  "properties": {
+    "name": "Malmö"
+  },
+  "geometry": {
+    "type": "MultiPolygon",
+    "coordinates": [[13.1249, 55.6078], [13.1154, 55.6077], [13.1151, 55.6081], [13.1139, 55.6084], [13.1134, 55.6073], [13.1104, 55.6057], [13.1094, 55.6052], [13.1086, 55.6046], [13.1075, 55.6040], [13.1039, 55.6049], [13.0997, 55.6047], [13.0959, 55.6048], [13.0956, 55.6059], [13.0928, 55.6087], [13.0918, 55.6104], [13.0899, 55.6103], [13.0889, 55.6098], [13.0889, 55.6119], [13.0873, 55.6116], [13.0872, 55.6118], [13.0834, 55.6112], [13.0816, 55.6135], [13.0800, 55.6132], [13.0787, 55.6135], [13.0765, 55.6136], [13.0742, 55.6123], [13.0711, 55.6120], [13.0684, 55.6115], [13.0682, 55.6118], [13.0677, 55.6118], [13.0672, 55.6120], [13.0669, 55.6126], [13.0676, 55.6127], [13.0672, 55.6133], [13.0681, 55.6140], [13.0682, 55.6150], [13.0677, 55.6176], [13.0680, 55.6189], [13.0697, 55.6192], [13.0685, 55.6219], [13.0662, 55.6282], [13.0610, 55.6301], [13.0629, 55.6317], [13.0517, 55.6358], [13.0402, 55.6378], [13.0171, 55.6410], [13.0171, 55.6410], [12.9724, 55.6797], [12.9724, 55.6797], [12.8765, 55.6642], [12.8765, 55.6642], [12.8959, 55.6433], [12.8814, 55.6142], [12.7294, 55.5409], [12.7153, 55.4892], [12.7153, 55.4892], [12.9230, 55.5044], [12.9230, 55.5044], [13.0477, 55.5142], [13.0569, 55.5254], [13.1086, 55.5272], [13.1086, 55.5272], [13.1374, 55.5452], [13.1211, 55.5616], [13.1500, 55.5714], [13.1487, 55.6041], [13.1487, 55.6041], [13.1249, 55.6078]]
+  }
+};
+
+var storp = {
+  "type": "Feature",
+  "bbox": [13.0997, 55.6041, 13.3556, 55.7052],
+  "properties": {
+    "name": "Staffanstorp"
+  },
+  "geometry": {
+    "type": "MultiPolygon",
+    "coordinates": [[13.2964, 55.6064], [13.2806, 55.6203], [13.2610, 55.6233], [13.2367, 55.6152], [13.1982, 55.6180], [13.1880, 55.6057], [13.1487, 55.6041], [13.1487, 55.6041], [13.1249, 55.6078], [13.1249, 55.6078], [13.1302, 55.6179], [13.1305, 55.6202], [13.1248, 55.6204], [13.1252, 55.6211], [13.1246, 55.6216], [13.1235, 55.6209], [13.1228, 55.6208], [13.1212, 55.6203], [13.1207, 55.6208], [13.1196, 55.6207], [13.1192, 55.6200], [13.1161, 55.6194], [13.1156, 55.6201], [13.1139, 55.6202], [13.1134, 55.6211], [13.1121, 55.6211], [13.1108, 55.6261], [13.1095, 55.6317], [13.1126, 55.6324], [13.1153, 55.6337], [13.1193, 55.6335], [13.1203, 55.6353], [13.1199, 55.6355], [13.1201, 55.6362], [13.1168, 55.6369], [13.1183, 55.6384], [13.1190, 55.6401], [13.1197, 55.6441], [13.1218, 55.6443], [13.1233, 55.6497], [13.1272, 55.6481], [13.1335, 55.6528], [13.1319, 55.6534], [13.1366, 55.6554], [13.1311, 55.6585], [13.1185, 55.6583], [13.1167, 55.6620], [13.1120, 55.6662], [13.1120, 55.6662], [13.1158, 55.6736], [13.1013, 55.6744], [13.1002, 55.6848], [13.0997, 55.6969], [13.1181, 55.7037], [13.1181, 55.7037], [13.1401, 55.7052], [13.1487, 55.6997], [13.1550, 55.6974], [13.1594, 55.6930], [13.1661, 55.6895], [13.1711, 55.6862], [13.1688, 55.6805], [13.1739, 55.6800], [13.1779, 55.6793], [13.1829, 55.6770], [13.2067, 55.6751], [13.2784, 55.6881], [13.3180, 55.6482], [13.3508, 55.6363], [13.3556, 55.6178], [13.3350, 55.6124], [13.3181, 55.6223], [13.2964, 55.6064]]
+  }
+};
+
+var vell = {
+  "type": "Feature",
+  "bbox": [12.6106, 55.1350, 13.1722, 55.5272],
+  "properties": {
+    "name": "Vellinge"
+  },
+  "geometry": {
+    "type": "MultiPolygon",
+    "coordinates": [[13.1669, 55.4832], [13.1620, 55.4725], [13.1342, 55.4564], [13.1014, 55.4656], [13.0800, 55.4648], [13.0697, 55.4663], [13.0520, 55.4645], [13.0598, 55.4349], [13.0711, 55.4250], [13.0432, 55.4183], [13.0136, 55.3807], [13.0136, 55.3807], [12.8761, 55.1350], [12.8761, 55.1350], [12.7949, 55.1667], [12.6772, 55.2500], [12.6389, 55.3083], [12.6420, 55.3373], [12.6420, 55.3373], [12.6106, 55.4317], [12.6106, 55.4317], [12.7153, 55.4892], [12.7153, 55.4892], [12.9230, 55.5044], [12.9230, 55.5044], [13.0477, 55.5142], [13.0569, 55.5254], [13.1086, 55.5272], [13.1086, 55.5272], [13.1432, 55.4909], [13.1722, 55.4891], [13.1669, 55.4832]]
+  }
+};
+
+GM.Boundary = function (name, coordSet) {
+  this.name = name;
+  var poly = this.polygon = [];
+  for (var i = 0, ii = coordSet.length; i < ii; i++) {
+    var polyCoords = coordSet[i];
+    poly[i] = [];
+    for (var j = 0, jj = polyCoords.length; j < jj; j++) {
+      var el = polyCoords[j];
+      poly[i].push({
+        lon: el[0],
+        lat: el[1]
+      });
+    }
+  }
+};
+
+GM.createMap = function (lat, lon, zoomlevel, id) {
+  var opts = {
+    center: new google.maps.LatLng(lat, lon),
+    zoom: zoomlevel,
+    scrollwheel: false,
+    streetViewControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var el = document.getElementById(id);
+  return new google.maps.Map(el, opts);
+};
+
+GM.mapController = {
+  map: null,
+  activeBoundaryPoints: null,
+  activeBoundaryPolygon: null,
+  overlay: null,
+
+  init: function (map) {
+    this.map = map;
+    this._helper = new google.maps.OverlayView();
+    this._helper.draw = $.noop;
+    this._helper.onAdd = $.noop;
+    this._helper.onRemove = $.noop;
+    this._helper.setMap(map);
+    return this;
+  },
+
+  fromLatLngToDivPixel: function (p) {
+    return this._helper.getProjection().fromLatLngToDivPixel(p);
+  },
+
+  setMarkers: function (markers) {
+    this.overlay = new GM.MarkerOverlay(this.map, markers);
+  },
+
+  setBounds: function (boundary) {
+    this.activeBoundaryPoints = boundary == null ? null : boundary.polygon;
+  },
+
+  moveMarkers: function (markers) {
+    for (var i = 0, ii = markers.length; i < ii; i++) {
+      var mrk = markers[i];
+      var currentMarker = document.getElementById(mrk.id);
+      if (currentMarker) {
+        var pos = this.fromLatLngToDivPixel(mrk.pos);
+        currentMarker.style.left = pos.x + 'px';
+        currentMarker.style.top = pos.y + 'px';
+      }
+    }
+
+  },
+
+  drawBounds: function () {
+    if (this.activeBoundaryPoints == null) {
+      return;
+    }
+    this.clearBounds();
+    var shadePoly = [[
+      new google.maps.LatLng(-89, -50),
+      new google.maps.LatLng(-89, 80),
+      new google.maps.LatLng(89, 80),
+      new google.maps.LatLng(89, -50)
+    ]];
+    for (var i = 0, len = this.activeBoundaryPoints.length; i < len; i++) {
+      var item = this.activeBoundaryPoints[i];
+      var holePoly = [];
+      for (var j = 0, jj = item.length; j < jj; j++) {
+        var c = item[j];
+        holePoly.push(new google.maps.LatLng(c.lat, c.lon));
+      }
+      holePoly.reverse();
+      shadePoly.push(holePoly);
+    }
+
+    this.activeBoundaryPolygon = new google.maps.Polygon({
+      paths: shadePoly,
+      fillColor: "#aa7733",
+      fillOpacity: 0.5,
+      strokeWeight: 1,
+      strokeColor: "#aa7733",
+      strokeOpacity: 0.8,
+      map: this.map,
+      zIndex: 0
+    });
+  },
+  clearBounds: function () {
+    if (this.activeBoundaryPolygon) {
+      this.activeBoundaryPolygon.setMap(null);
+    }
+  }
+};
+
+$(function () {
+  var map = GM.createMap(55.588047, 13.000946, 10, 'gmap');
+  var c = GM.mapController.init(map);
+
+  var mlm = new GM.Boundary('Malmö', [ malmo.geometry.coordinates, storp.geometry.coordinates]);
+  c.setBounds(mlm);
+  c.drawBounds();
+
+  var markers = [];
+  var id = 0;
+  _.each(munis, function (o) {
+    markers.push(new GM.ThinMarker(
+      'home_' + (id++),
+      new google.maps.LatLng(o.lat, o.lon),
+      null,
+      'clusterMarker'
+    ));
+  });
+
+  c.setMarkers(markers);
+
+  function movePos(pos) {
+    var lat = pos.lat() + ((Math.random() * 2) - 1) / 10;
+    var lon = pos.lng() + ((Math.random() * 2) - 1) / 10;
+    return new google.maps.LatLng(lat, lon);
+  }
+
+  $('#animate').click(function () {
+    _.each(markers, function (mrk) {
+      mrk.pos = movePos(mrk.pos);
+    });
+    c.moveMarkers(markers);
+  });
+
+});
+
+
+/******************************************************************************/
+
+GM.MarkerOverlay = function (map, markers) {
+  this.map = map;
+  this.setMap(map);
+  this.markers = markers;
+};
+
+GM.MarkerOverlay.prototype = new google.maps.OverlayView();
+
+_.extend(GM.MarkerOverlay, Backbone.Events);
+
+_.extend(GM.MarkerOverlay.prototype, {
+  onAdd: function () {
+    this.el = document.createElement('div');
+    var panes = this.getPanes();
+    panes.overlayMouseTarget.appendChild(this.el);
+  },
+
+
+  onRemove: function () {
+    this.markers = [];
+    this.el.parentNode.removeChild(this.el);
+    this.el = null;
+  },
+
+
+  draw: function () {
+    var i = this.markers.length;
+    var HTML = '';
+    var proj = this.getProjection();
+    var pos;
+    var marker;
+    /* Hmmm... seems like string concatenation is fastest after all
+     * http://jsperf.com/ultimate-string-concatenation-tests/3 */
+    while (i--) {
+      marker = this.markers[i];
+      if (marker.visible) {
+        pos = proj.fromLatLngToDivPixel(marker.pos);
+        HTML += '<div id="' + marker.id + '" style="left:';
+        HTML += pos.x;
+        HTML += 'px; top:';
+        HTML += pos.y;
+        HTML += 'px; z-index: 10000;"';
+        HTML += ' class="' + marker.className + '"';
+        HTML += '>';
+        if (marker.text) {
+          HTML += marker.text;
+        }
+        HTML += '</div>';
+      }
+    }
+    this.el.innerHTML = HTML;
+  },
+
+
+  getMarkers: function () {
+    return this.markers;
+  }
+});
+
+GM.ThinMarker = function (id, pos, text, className) {
+  this.id = id;
+  this.pos = pos;
+  this.text = text;
+  this.className = className;
+  this.visible = true;
+};
+_.extend(GM.ThinMarker.prototype, {
+
+  getPosition: function () {
+    return this.pos;
+  },
+
+  getMap: $.noop,
+
+  setMap: function (a) {
+    this.visible = a === null ? false : true;
+  }
+});
